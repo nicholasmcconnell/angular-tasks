752bbbee684db5b7275137fe4f40271671d13b7e
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2021-12-14 12:31:14 -0600

    imported angular-font awesome package.  Added faTimes icon to task-item component

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Task } from '../../Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
@@ -7,6 +8,8 @@ import { Task } from '../../Task';
 })
 export class TaskItemComponent implements OnInit {
   @Input() task!: Task
+
+  faTimes = faTimes;
 
   constructor() { }
 
