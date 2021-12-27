ebb4661aa0e7ab325b74601df7599827c9069a13
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2021-12-27 14:58:34 -0600

    added logic to ui services to subscript to button click to togglet bolean dictating the show hide featyre of  the add task form

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 
 import { Task } from '../../Task';
 
@@ -14,11 +16,15 @@ export class AddTaskComponent implements OnInit {
   text: string = '';
   day: string = '';
   reminder: boolean = false;
+  showAddTask: boolean = false;
+  subscription: Subscription;
 
   @Output() onAddTask: EventEmitter<Task> = new EventEmitter;
 
 
-  constructor() { }
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe((value) => this.showAddTask = value);
+  }
 
   ngOnInit(): void {
   }
